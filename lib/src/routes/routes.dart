27a9e20f4213e04e52ea38b7ec94570118751624
CristianX <thecristianx@hotmail.com>
2020-04-30 15:06:02 -0500

    Implementando página de animated_container para mostrar un AnimatedContainer

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -6,6 +6,8 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
 
 
 // Para transformarlo a mapa
@@ -17,6 +19,8 @@ return <String, WidgetBuilder>{
   'alert' : ( BuildContext context ) => AlertPage(),
   'avatar': ( BuildContext context ) => AvatarPage(),
   'card': ( BuildContext context ) => CardPage(),
+  'inputs': ( BuildContext context ) => InputPage(),
+  'animatedContainer': ( BuildContext context ) => AnimatedContainerPage(),
 };
 
 
