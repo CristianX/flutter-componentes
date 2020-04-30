c33ecaeb7b1f19105288e60a62e5586ff156dd15
CristianX <thecristianx@hotmail.com>
2020-04-30 15:36:36 -0500

    Implementando inputfield en la vista de input_page.dart

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -6,10 +6,62 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  String _nombre = '';
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs de texto'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric( horizontal: 10.0, vertical: 20.0 ),
+        children: <Widget>[
+          _crearInput(),
+          Divider(),
+          _crearPersona()
+        ],
+      ),
     );
+  }
+
+  Widget _crearInput() {
+
+    return TextField(
+
+      // autofocus: true,
+
+      // Capitalización por espacios
+      textCapitalization: TextCapitalization.words,
+      decoration: InputDecoration(
+        counter: Text('Caractéres ${ _nombre.length }'),
+        hintText: 'Nombre completo',
+        labelText: 'Nombre',
+        helperText: 'Nombre completo',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle),
+        // Bordes del contenedor
+        border: OutlineInputBorder( 
+          borderRadius: BorderRadius.circular(20.0)
+         ),
+      ),
+      // Capturando valor de la caja de texto
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+        });
+      }
+
+    );
+
+  }
+
+  Widget _crearPersona() {
+
+    return ListTile(
+      title: Text('Nombre: $_nombre'),
+    );
+
   }
 }
