4f513502865098d49621dc1d05f24796c562bb54
CristianX <thecristianx@hotmail.com>
2020-04-30 19:35:38 -0500

    Añadiendo el infiniteScroll con cálculo de pixeles

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -7,7 +7,34 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
 
-  List<int> _listaNumeros = [1,2,3,4,5];
+  // Controla el scroll a voluntad
+  ScrollController  _scrollController = new ScrollController();
+
+  List<int> _listaNumeros = new List();
+
+  // Para agregar más imagenes
+  int _ultmoItem = 0;
+
+  // Etapa del ciclo de vida de la creación de un StateFulWidget
+  @override
+  void initState() { 
+    super.initState();
+
+    _agregar10();
+
+
+    // Se disparara cada vez que se mueva el scroll
+    _scrollController.addListener(() {
+      
+      // Analizando si llega al final
+      // _scrollController.position.maxScrollExtent es largo máximo
+      if( _scrollController.position.pixels == _scrollController.position.maxScrollExtent ) {
+        _agregar10();
+      }
+
+    });
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +48,8 @@ class _ListaPageState extends State<ListaPage> {
 
   Widget _crearLista(){
     return ListView.builder(
+      // Controlando scroll
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: ( BuildContext context, int index ){
 
@@ -36,4 +65,20 @@ class _ListaPageState extends State<ListaPage> {
       },
     );
   }
+  // Al llamar al método, agrega 10 imagenes a la lista
+  _agregar10() {
+
+
+    setState(() {
+      
+      for (var i = 0; i < 10; i++) {
+
+        _ultmoItem ++;
+        _listaNumeros.add( _ultmoItem );
+        
+      }
+    });
+
+  }
+
 }
